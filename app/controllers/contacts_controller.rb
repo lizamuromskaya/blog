617e91fe19d2811ac18ce5cbc345a934cc4fafb6
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-07-16 14:16:32 +0300

    save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,14 @@
 class ContactsController < ApplicationController
+	def new
+	end
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
 end
